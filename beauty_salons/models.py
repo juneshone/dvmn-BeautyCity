ca38f398126c6d8110ca7822209c78a4a65db499
ZiganshinIB <elzig3012@gmail.com>
2024-07-24 17:58:47 +0300

    Test: add PayServise

--- beauty_salons/models.py
+++ beauty_salons/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pay(models.Model):
+    appointment = models.BigIntegerField(verbose_name="Номер записи", blank=True, null=True)
+    operation_id = models.CharField(verbose_name="ID операции", max_length=255, unique=True)
+    amount = models.DecimalField(verbose_name="Сумма", max_digits=10, decimal_places=2, blank=True, null=True)
+    is_success = models.BooleanField(verbose_name="Успешно", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f"Платеж {self.operation_id}"
--- beauty_salons/views.py
+++ beauty_salons/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseServerError
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+
+import json
+
+from .models import Pay
 
 
 def index(request):
@@ -34,6 +38,20 @@ def notes(request):
 @csrf_exempt
 @require_http_methods(['POST'])
 def save_pay(request):
-    data = request.POST
-    print(data)
+    cd = request.POST
+    print(cd)
+    try:
+        operation_id = int(cd.get('operation_id')[0])
+        amount = round(float(cd.get('amount')[0]), 2)
+        is_success = cd.get('unaccepted')[0] == 'false'
+        appointment = int(cd.get('label')[0])
+        Pay.objects.create(
+            operation_id=operation_id,
+            amount=amount,
+            is_success=is_success,
+            appointment=appointment
+        )
+    except Exception as e:
+        print(e)
+        return HttpResponseServerError('Error save pay')
     return JsonResponse({'status': 'ok'})
