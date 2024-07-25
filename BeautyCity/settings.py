5b2266a22faddc1c38bf9589e22450f0f72e6d2b
ZiganshinIB <elzig3012@gmail.com>
2024-07-25 08:20:00 +0300

    Russification website

--- BeautyCity/settings.py
+++ BeautyCity/settings.py
@@ -111,9 +111,9 @@ AUTH_USER_MODEL = 'beauty_salons.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
--- beauty_salons/apps.py
+++ beauty_salons/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BeautySalonsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "beauty_salons"
+    verbose_name='Салон красоты'
